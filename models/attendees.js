f12ca31d4217d967452a7a38c8318189804dad3d
Ian Juma <ian.juma@yahoo.com>
2015-02-06 17:04:26 +0300

    toulon update

--- models/attendees.js
+++ models/attendees.js
@@ -1,12 +1,12 @@
 var config = require('../config/database');
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize('database', 'username', 'password');
+var sequelize = new Sequelize(config.postgres);
 
 
 var Attendees = sequelize.define('Attendees', {
   name: Sequelize.STRING,
   email: Sequelize.STRING,
-  date_registered: Sequelize.DATE,
+  date_registered: { Sequelize.DATE, defaultValue: new Date() },
   idea: Sequelize.TEXT,
   phone_number: Sequelize.INTEGER,
   github_username: Sequelize.STRING
--- routes/attendees.js
+++ routes/attendees.js
@@ -1,41 +1,29 @@
 var slug = require('slug');
-var Post = require('../models/posts');
+var Attendees = require('../models/attendees');
 
 
-exports.addPost = function(req, res) {
+exports.addAttendee = function(req, res) {
 	if ( ! req.is('application/json') ) {
 		res.status(400).json({ 'Error': 'Bad Request' });
 	}
 
-	var new_post = new Post({
-		id: req.body.id,
-	    title: req.body.title,
-	    _slug: slug((req.body.title).toLowerCase()),
-	    content: req.body.content,
-	    profileImage: req.body.profileImage,
-	    type: req.body.post_type,
-    	displayName: req.body.displayName,
-    	username: req.body.username,
-	    postmetadata: {
-    		comments_id: req.body.comments_id,
-  			time_to_read: req.body.time_to_read,
-  			likes: req.body.likes
-	    }
+	var new_post = new Attendees({
+	  name: req.body.name,
+	  email: req.body.email,
+	  idea: req.body.idea,
+	  phone_number: req.body.phone_number,
+	  github_username: req.body.github_username
 	});
 
-	new_post.save(function(error, result) {
-		if (result == null) {
-			res.status(400).json({ "Error": "Post Already Exists" });
-		}
+	Attendees.save().then(function(error) {
 	    if (error) {
 	        res.status(500).json({ error: "something blew up, we're fixing it" });
 	    }
 	    else {
-	        console.log('Post Saved');
-        	res.set({
+	        console.log('Attendee saved');
+	        res.set({
 			  'Content-Type': 'application/json',
 			});
-
 			res.status(200).json({ 'OK': 'Post Created'});
 	    }
 	});
@@ -63,74 +51,5 @@ exports.getPostById = function(req, res) {
 
 
 exports.getPosts = function(req, res) {
-
-	Post.orderBy("creation_date").run(function(error, result) {
-	    if (error) {
-	        res.status(500).json({ error: "something blew up, we're fixing it" });
-	    }
-	    else {
-	        console.log('Post sent');
-	        res.set({
-			  'Content-Type': 'application/json',
-			});
-
-			res.status(200).json(result);
-	    }
-	});
-};
-
-
-exports.deletePostById = function(req, res) {
-
-	Post.get( req.params.id ).delete().run(function(error, result) {
-		if (result == null) {
-			res.status(404).json({ "Error": "Post Not Found" });
-		}
-		if (error) {
-			res.status(500).json({ "error": "something blew up, we're fixing it" });
-		} else {
-	        console.log('Post deleted');
-	        res.set({
-			  'Content-Type': 'application/json'
-			});
-
-			res.status(200).json({ 'OK': 'Post Deleted' });
-		}
-	});
-};
-
-
-exports.updatePostById = function(req, res) {
-
-	var _post = new Post({
-		id: req.body.id,
-	    title: req.body.title,
-	    _slug: slug((req.body.title).toLowerCase()),
-	    content: req.body.content,
-	    profileImage: req.body.profileImage,
-	    type: req.body.post_type,
-    	displayName: req.body.displayName,
-    	username: req.body.username,
-	    postmetadata: {
-    		comments_id: req.body.comments_id,
-  			time_to_read: req.body.time_to_read,
-  			likes: req.body.likes
-	    }
-	});
-
-	Post.get( req.params.id ).update(_post).run(function(error, result) {
-		if (result == null) {
-			res.status(404).json({ "Error": "Post Not Found" });
-		}
-		if (error) {
-			res.status(500).json({ "error": "something blew up, we're fixing it" });
-		} else {
-	        console.log('Post updated');
-	        res.set({
-			  'Content-Type': 'application/json'
-			});
-
-			res.status(200).json(_post);
-		}
-	});
+	
 };
