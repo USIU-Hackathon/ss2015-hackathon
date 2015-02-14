ea64b48eea65705b338c087faeab697bba2bcba4
Paul Bombo <paul.bombo@gmail.com>
2015-02-14 12:36:59 +0300

    Work on registration page ajax

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,15 +1,40 @@
 $(function() {
 
-	if($('.content').hasClass('schedule')) {
+    if ($('.content').hasClass('schedule')) {
 
-	    window.onscroll = function() {
-	        if ($(window).scrollTop() < 120) {
-	            $('section > div > header').removeClass('sticky');
-	            $('footer').removeClass('shadow');
-	    } else {
-	            $('section > div > header').addClass('sticky');
-	            $('footer').addClass('shadow');
-	        }
-	    }
-	}
+        window.onscroll = function() {
+            if ($(window).scrollTop() < 120) {
+                $('section > div > header').removeClass('sticky');
+                $('footer').removeClass('shadow');
+            } else {
+                $('section > div > header').addClass('sticky');
+                $('footer').addClass('shadow');
+            }
+        }
+    }
+
+    //Registration form
+    $('#registration-form').on('submit', function(e) {
+        e.preventDefault();
+
+        var data = {
+            name: $('#registration-form input[name="name"]').val(),
+            id: $('#registration-form input[name="email"]').val(),
+            idea: $('#registration-form textarea[name="description"]').val(),
+            phone_number: $('#registration-form input[name="telephone"]').val(),
+            github_username: $('#registration-form input[name="github_username"]').val(),
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/addAttendee',
+            data: data,
+            success: function(res) {
+                console.log(res);
+            }
+
+        })
+    });
+
+
 })
