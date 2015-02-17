f2b2ec0713e0859d93e1479ef47c1a8ea586228c
Ian Juma <ian.juma@yahoo.com>
2015-02-17 18:41:58 +0300

    meta tags

--- routes/index.js
+++ routes/index.js
@@ -3,36 +3,43 @@ var router = express.Router();
 
 
 exports.index = function(req, res) {
-	res.render('index', { title: 'Hackathon' });
+	res.render('index', { title: 'Hackathon',
+		description: 'USIU-A PRESENTS A HACKATHON ON MARCH 14' });
 };
 
 
 exports.schedule = function(req, res) {
-	res.render('schedule', { title: 'Schedule' });
+	res.render('schedule', { title: 'Schedule',
+		description: 'Schedule for the USIU-A Hackathon' });
 };
 
 
 exports.privacy = function(req, res) {
-	res.render('privacy', { title: 'Privacy' });
+	res.render('privacy', { title: 'Privacy',
+		description: 'Privacy terms for the USIU-A Hackathon' });
 };
 
 exports.rules = function(req, res) {
-	res.render('rules', { title: 'Rules' });
+	res.render('rules', { title: 'Rules',
+		description: 'Rules for the USIU-A Hackathon' });
 };
 
 
 exports.terms = function(req, res) {
-	res.render('terms', { title: 'Terms and Conditions' });
+	res.render('terms', { title: 'Terms and Conditions',
+		description: 'Terms and Conditions for the USIU-A Hackathon' });
 };
 
 
 exports.apply = function(req, res) {
-	res.render('register', { title: 'Register' });
+	res.render('register', { title: 'Register',
+		description: 'Register for the USIU-A Hackathon' });
 };
 
 
 exports.contact = function(req, res) {
-	res.render('contact', { title: 'Contact' });
+	res.render('contact', { title: 'Contact',
+		description: 'Contact page for the USIU-A Hackathon' });
 }
 
 /*
@@ -80,7 +87,8 @@ exports.guide = function(req, res) {
     	//console.log(data.toString());
     	guide = data.toString();
 		guide = converter.makeHtml(guide);
-		res.render('guide', {title: 'Guide', guide: guide });
+		res.render('guide', {title: 'Guide', guide: guide,
+			description: 'Guide to the USIU-A Hackathon' });
 	});
 }
 
