c194f5e46e892a5d93b854132f1e0eaf28a25764
Ian Juma <ian.juma@yahoo.com>
2015-03-09 11:51:38 +0300

    mails added

--- mail.js
+++ mail.js
@@ -1,10 +1,22 @@
 var kueConfig = require('./config/workerQueue');
 var kue = require('kue')
   , redis = require('redis')
+  , r = require('rethinkdb')
   , jobs = kue.createQueue(kueConfig.kue);
 
+// rethink query - produce result and add create task
 
-exports.sendEmail = function(user) {
+var config = require('../config/database');
+r.connect(config.rethinkdb, function(err, conn) {
+	conn.use('hackathon');
+	r.db('hackathon').table('Attendee').run(conn, function() {
+
+	});
+})
+
+
+
+exports.sendInfoEmail = function(user) {
 
 	var job = jobs.create('email', {
 	    to: user.email,
